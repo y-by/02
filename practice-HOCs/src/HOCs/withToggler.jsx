f0173ab3aebfdab7dfb2dcdd17ecea020cd04dec
janay <janayby@gmail.com>
2023-02-09 09:41:57 +0200

    change component declaration toBe more flexible

--- practice-HOCs/src/HOCs/withToggler.jsx
+++ practice-HOCs/src/HOCs/withToggler.jsx
@@ -17,9 +17,9 @@ class Toggler extends Component {
   }
 
   render() {
-    const Component = this.props.component
+    const {component: Component, defaultOnValue, ...props} = this.props
     return (
-      <Component on={this.state.on} toggle={this.toggle} {...this.props}/>
+      <Component on={this.state.on} toggle={this.toggle} {...props}/>
     )
   }
 }
