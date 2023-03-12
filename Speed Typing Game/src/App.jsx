679299c207a5fec3a643333152c38d2a0d8c347c
janay <janayby@gmail.com>
2023-03-12 22:23:32 +0200

    add state and handleChange

--- Speed Typing Game/src/App.jsx
+++ Speed Typing Game/src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  const [text, setText] = useState("")
+  
+  function handleChange(e) {
+      const {value} = e.target
+      setText(value)
+  }
+  
+  console.log(text)
+  
   return (
-    <div className="App">
-      <h1>How fast do you type?</h1>
-      <textarea defaultValue="textArea - value as defaultValue"/>
-      <h4>H4 - Time reminaing: ???</h4>
-      <button>Start</button>
-      <h1>Word count: ???</h1>
-    </div>
+      <div>
+          <h1>How fast do you type?</h1>
+          <textarea
+              onChange={handleChange}
+              value={text}
+          />
+          <h4>Time remaining: ???</h4>
+          <button>Start</button>
+          <h1>Word count: ???</h1>
+      </div>
   )
 }
 
