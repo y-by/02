60e4d692bb707376d254a844104cc72f9f8b2562
janay <janayby@gmail.com>
2023-03-22 19:52:02 +0200

    initial commit

---
+++ Theme switcher/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+import Button from './Button'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Button />
+    </div>
+  )
+}
+
+export default App
